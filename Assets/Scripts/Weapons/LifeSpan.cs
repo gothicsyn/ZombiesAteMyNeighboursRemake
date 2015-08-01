f8a67f1cc1923e6688fce87f69a898da6e3c1a37
Mark Plant <gothicsyn@live.co.uk>
2015-08-02 00:35:39 +0100

    Quick update that will now destroy created "bullets" after a set time to ensure the system isnt bogged down by stray prefabs and the likes

---
+++ Assets/Scripts/Weapons/LifeSpan.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifeSpan : MonoBehaviour {
+
+	public int 			lifetime	 = 45;			// Lifetime of object
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Destroy(gameObject, lifetime);
+	}
+}
