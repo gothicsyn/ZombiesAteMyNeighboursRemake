a2d06a088d4adb415492f62be57d3b801c8aa8fa
Mark Plant <gothicsyn@live.co.uk>
2015-08-02 00:20:22 +0100

    First new feature update adding in the ability to shoot a weapon. This builds the first weapon the WaterGun.

---
+++ Assets/Scripts/Weapons/Bullet.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public Rigidbody 	projectile;			// Will require a rigidbody prefab to act as a prejectile.  
+	public float 		speed;				// Sets the objects speed travelling away from the spawn
+	public AudioClip	SFX;				// Add a desired sound effect
+	AudioSource 		audio;
+
+	// Use this for initialization
+	void Start () {
+		audio =  GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	// Keyboard Input
+		if(Input.GetKeyUp("space")) {
+			Rigidbody instantiatedProjectile = Instantiate(projectile,			// Projectile 
+			                                               transform.position, 	// Position in the world
+			                                               transform.rotation) 	// Rotation in the world
+															as Rigidbody;		// Converts back to rigidbody object
+
+		// Make the oject move through the world
+			instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed));
+			audio.PlayOneShot(SFX, 0.7F);
+
+		// Turn off collisions with root object
+			Physics.IgnoreCollision(instantiatedProjectile.GetComponent<Collider>(), transform.root.GetComponent<Collider>());
+		}
+	}
+}
