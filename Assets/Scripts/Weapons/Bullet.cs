196306003995b66e850ae1524b56f68fe760212d
Mark Plant <gothicsyn@live.co.uk>
2016-04-01 14:45:56 +0100

    MinorChanges

--- Assets/Scripts/Weapons/Bullet.cs
+++ Assets/Scripts/Weapons/Bullet.cs
@@ -6,7 +6,7 @@ public class Bullet : MonoBehaviour {
 	public Rigidbody 	projectile;			// Will require a rigidbody prefab to act as a prejectile.  
 	public float 		speed;				// Sets the objects speed travelling away from the spawn
 	public AudioClip	SFX;				// Add a desired sound effect
-	new AudioSource 		audio;
+	new AudioSource 	audio;
 
 	// Use this for initialization
 	void Start () {
@@ -18,16 +18,22 @@ public class Bullet : MonoBehaviour {
 	// Keyboard Input
 		if(Input.GetKeyUp("space")) {
 			Rigidbody instantiatedProjectile = Instantiate(projectile,			// Projectile 
-			                                               transform.position, 	// Position in the world
-			                                               transform.rotation) 	// Rotation in the world
-															as Rigidbody;		// Converts back to rigidbody object
+			                                                transform.position, 	// Position in the world
+			                                        	    transform.rotation) 	// Rotation in the world
+														 	as Rigidbody;		// Converts back to rigidbody object
 
 		// Make the oject move through the world
 			instantiatedProjectile.velocity = transform.TransformDirection(new Vector3(0, 0, speed));
 			audio.PlayOneShot(SFX, 0.7F);
 
 		// Turn off collisions with root object
-			Physics.IgnoreCollision(instantiatedProjectile.GetComponent<Collider>(), transform.root.GetComponent<Collider>());
+//			Physics.IgnoreCollision(instantiatedProjectile.GetComponent<Collider>(), transform.root.GetComponent<Collider>());
+		}
+	}
+
+	void OnTriggerEnter(Collider otherObject){
+		if (otherObject.tag == "Enemy") {
+			Debug.LogWarning("No Code Dealing With Enemy Death");
 		}
 	}
 }
