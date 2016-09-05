60b7baac107b08eba111e40c76d353137d2c140a
Mark Plant <gothicsyn@live.co.uk>
2016-09-05 14:28:26 +0100

    Update0.5a
    Removed obsolete scripts, updated the new weapon script, hopefully I can
    clone this across the board of weapons.

---
+++ Assets/Scripts/Weapons/WaterGun.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterGun : MonoBehaviour {
+
+	public Transform muzzle;
+	public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonUp (0)) {
+			Shoot();
+		}
+	}
+
+	void Shoot () {
+		Instantiate (prefab, muzzle.position, Quaternion.identity);
+	}
+}
