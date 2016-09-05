b244f422468da26f229daf5d02e88109ec5801ad
Mark Plant <gothicsyn@live.co.uk>
2016-09-05 13:15:49 +0100

    Update0.5
    Began reworking some of the AI, Ai now tracks the player which is not
    quite how I want it to work but it will do for now.

---
+++ Assets/Scripts/Controllers/Spawn_Point.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn_Point : MonoBehaviour {
+
+	public GameObject prefab;
+	public float repeatTime = 3f;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("Spawn", 2f, repeatTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void Spawn () {
+		Instantiate (prefab, transform.position, Quaternion.identity);
+	}
+}
---
+++ Assets/Scripts/Enemy/AI_Zombie.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI_Zombie : MonoBehaviour {
+
+	public NavMeshAgent agent;
+	public Transform target;
+
+	void Awake () {
+		target = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+
+	// Use this for initialization
+	void Start () {
+		agent.SetDestination (target.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
